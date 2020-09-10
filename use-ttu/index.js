e4e4429505e8487342af8e686e3dcc82af7b7778
dvhungg123@gmail.com <dvhungg123@gmail.com>
2020-09-10 08:58:59 +0700

    update

---
+++ use-ttu/index.js
@@ -0,0 +1,8 @@
+let inputWidth
+let inputHeight
+inputWidth = prompt('Enter the width')
+inputHeight = prompt('Enter the heigh')
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
